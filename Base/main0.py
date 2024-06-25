7e09c00784547435e43f8f5aca5ec78fd909b05b
arukaff <a.v.rukavishnikov@gmail.com>
2024-06-25 19:14:16 +0300

    work

--- Base/main0.py
+++ Base/main0.py
@@ -17,28 +17,47 @@
 # n=int(input('Введите число N: '))
 # print('Количество способов разложения числа N: ',f(n,n))
 
-#без рекурсии
+#без рекурсии считает и разписывает правильно только до 10
 n=10
-i=0
 ls=[]
 k=n
 while k>0:
-    s=[0 for i in range(n+1)]
+    s=[0 for i in range(n+2)]
     s[0]=k
-    for i in range(1,n+1):
+    for i in range(1,n+2):
         s[i]=n-sum(s)
-        if s[i]==0 and s[0:-1] not in ls:
+        if s[i]==0 and s[0:-2] not in ls:
             if s[i-1]<=s[0]:
-                ls.append(s[0:-1])
-                for j in range(1,i):
+                ls.append(s[0:-2])
+                for j in range(i-1,0,-1):
                     if s[j]>1:
                         s[j]-=1
-                        s.insert(j+1,1)
-                        del s[-1]
+                        if s[j]>s[j+1]:
+                            s[j+1]+=1
+                            ii=i-(j+1)
+                            ss=s[ii:i]
+                            su=sum(s[ii:i])
+                            if su==s[ii-1]:
+                                s[ii]=su
+                                s[ii+1:]=map(lambda x: x*0,s[ii+1:])
+                            # if n-sum(s)==0:
+                            #     break
+                            # su=n-sum(s[:j+1])
+                            # if s[j]>=su:
+                            #     s[j+1]=sum(s[j+1:])
+                            #     s[j+2:]=map(lambda x: x*0,s[j+2:])
+                            # else:
+                            #     s[j+2]=sum(s[j+2:])
+                            #     s[j+3:]=map(lambda x: x*0,s[j+3:])
+                            #break
+                        else:
+                            s.insert(j+1,1)
+                            del s[-1]
                         break
             else:
                 s[i]=s[i-1]-s[0]
                 s[i-1]=s[0]
     k-=1
-print(ls)
 print(len(ls))
+for i in ls:
+    print(i)
